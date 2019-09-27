a00857bb08bf8721596e6b167cbb0562ad16d838
omgitsgod <omgitsgod@gmail.com>
2019-09-27 17:20:24 -0400

    Add status

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const routes = require('express').Router();
 
 routes.get('/', (req, res, next) => {
   console.log('Accessing Index');
-  res.send('Accessing Index');
+  res.status(200).send('Accessing Index');
 });
 
 module.exports = routes;
