23263dfc71919ce2ad12603a3f23e547113d1afa
omgitsgod <omgitsgod@gmail.com>
2019-09-27 17:23:08 -0400

    Add content type

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const routes = require('express').Router();
 
 routes.get('/', (req, res, next) => {
   console.log('Accessing Index');
+  res.set('Content-Type', 'text/html');
   res.status(200).send('Accessing Index');
 });
 
