2acfa3639513860efc48432e8b4c2aba86eaa100
omgitsgod <omgitsgod@gmail.com>
2019-09-29 18:34:45 -0400

    Add saveuser route

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const routes = require('express').Router();
 
 routes.get('/', (req, res, next) => {
   console.log('Accessing Index');
-  res.set('Content-Type', 'text/html');
+  res.status(200).send('Vinyasa Flow');
+});
+
+routes.get('/saveUser', (req, res, next) => {
+  console.log(req.body);
   res.status(200).send('Vinyasa Flow');
 });
 
